72f6aad84e1b9850a1e9e8b6a311cdf84d7494b3
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2022-05-01 05:28:53 +0200

    sauceAllRoutes

---
+++ backend/routes/sauce.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const sauceCtrl = require('../controllers/sauce');
+
+router.get('/', sauceCtrl.getAllSauces);
+router.get('/:id', sauceCtrl.getOneSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
+router.put('/:id', auth, multer, sauceCtrl.updateSauce);
+router.delete('/:id', auth, multer, sauceCtrl.deleteSauce);
+
+module.exports = router;
