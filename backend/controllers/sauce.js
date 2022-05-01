113e9e9ba0cbc06b3fa67523cd01f2bdfeed2f06
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2022-05-01 06:05:05 +0200

    createSauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -12,3 +12,14 @@ exports.getOneSauce = (req, res, next) => {
         .then(newSauce => res.status(200).json(newSauce))
         .catch(error => res.status(404).json({ error }));
 };
+
+exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce);
+    const newSauce = new sauce({ 
+        ...sauceObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    });
+    newSauce.save()
+        .then(() => res.status(201).json({ message: 'Nouvelle sauce insérée avec succès !' }))
+        .catch(error => res.status(400).json({ error }));
+};
