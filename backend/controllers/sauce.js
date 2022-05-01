112114303556d09e31bf07908d05c54ad54362b8
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2022-05-01 05:57:27 +0200

    getAll

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,2 +1,14 @@
 const fs = require('fs');
-const sauce = require('../models/sauce');
+const sauce = require('../models/Sauce');
+
+exports.getAllSauces = (req, res, next) => {
+    sauce.find()
+        .then(newSauce => res.status(200).json(newSauce))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneSauce = (req, res, next) => {
+    sauce.findOne({ _id: req.params.id })
+        .then(newSauce => res.status(200).json(newSauce))
+        .catch(error => res.status(404).json({ error }));
+};
